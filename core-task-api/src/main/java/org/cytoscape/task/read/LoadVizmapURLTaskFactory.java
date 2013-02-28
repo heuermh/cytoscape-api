0af524ce06c07ccd735086d2e91010a48bdd50fe
Michael Heuer <heuermh@acm.org>
2013-02-27 23:20:47 -0600

    adding LoadVizmapURLTaskFactory

---
+++ core-task-api/src/main/java/org/cytoscape/task/read/LoadVizmapURLTaskFactory.java
@@ -0,0 +1,58 @@
+package org.cytoscape.task.read;
+
+/*
+ * #%L
+ * Cytoscape Core Task API (core-task-api)
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2006 - 2013 The Cytoscape Consortium
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 2.1 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+import org.cytoscape.view.vizmap.VisualStyle;
+import org.cytoscape.work.TaskFactory;
+import org.cytoscape.work.TaskIterator;
+
+import java.net.URL;
+import java.util.Set;
+import java.io.InputStream;
+
+/**
+ * An interface for loading styles from URLs. 
+ * This interface also provides a task iterator for loading URLs into visual styles.
+ * @CyAPI.Api.Interface
+ * @CyAPI.InModule core-task-api
+ */
+public interface LoadVizmapURLTaskFactory extends TaskFactory {
+
+	/**
+	 * Return a set of VisualStyle objects read from the specified URL.
+	 * @param url The URL containing visual styles to be read.
+	 * @return a set of VisualStyle objects read from the specified URL.
+	 */
+	Set<VisualStyle> loadStyles(URL url);
+
+	/**
+	 * Creates a task iterator for loading URLs into visual styles.
+	 * The created task runs synchronously in the current thread and does not
+	 * create a task monitor.
+	 * @param file The file containing visual styles to be read.
+	 * @return A task iterator of type {@link TaskIterator}.
+	 */
+	TaskIterator createTaskIterator(URL url);
+}
